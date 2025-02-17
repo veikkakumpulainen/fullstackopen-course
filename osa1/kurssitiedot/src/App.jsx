522b119a068975273b8fb3131935fbf2078b1f58
veikkakumpulainen <weeikka@gmail.com>
2025-02-17 08:48:43 +0200

    1.1 paranneltu

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -12,7 +12,13 @@ const Content = (props) => {
   return (
     <div>
       <p>
-        {props.part} {props.exercises}
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
       </p>
     </div>
   );
@@ -39,9 +45,14 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
