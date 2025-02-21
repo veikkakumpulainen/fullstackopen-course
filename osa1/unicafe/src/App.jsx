12e2622de6503bbfd1e41ce4d5e02cbe0654a824
veikkakumpulainen <weeikka@gmail.com>
2025-02-21 07:37:54 +0200

    1.12 tehty

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -36,10 +36,21 @@ const Button = ({ onClick, text }) => {
 };
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [selected, setSelected] = useState(0);
+
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
   return (
     <div>
@@ -49,6 +60,13 @@ const App = () => {
       <Button onClick={() => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
+      <p>{anecdotes[selected]}</p>
+      <Button
+        onClick={() =>
+          setSelected(Math.floor(Math.random() * anecdotes.length))
+        }
+        text="next anecdote"
+      />
     </div>
   );
 };
