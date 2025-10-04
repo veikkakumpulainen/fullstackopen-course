76bf38c400c639194ecc883bd86552e1bfd13c4e
Veikka Kumpulainen <143580336+veikkakumpulainen@users.noreply.github.com>
2025-10-04 10:35:59 +0300

    3.8 tehty

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -26,7 +26,12 @@ let persons = [
 ]
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (request) => {
+  return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
