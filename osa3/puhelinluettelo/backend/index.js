fa99a667ec06f3ab54f7b706d49a0195a8cce0e8
Veikka Kumpulainen <143580336+veikkakumpulainen@users.noreply.github.com>
2026-05-20 18:56:08 +0300

    uibuild

--- osa3/puhelinluettelo/backend/index.js
+++ osa3/puhelinluettelo/backend/index.js
@@ -27,7 +27,7 @@ let persons = [
 ]
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static('dist'))
 
 morgan.token('body', (request) => {
   return JSON.stringify(request.body)
@@ -35,9 +35,9 @@ morgan.token('body', (request) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-// app.get('/', (request, response) => {
-//  response.send('<h1>Hello World!</h1>')
-// })
+ app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
